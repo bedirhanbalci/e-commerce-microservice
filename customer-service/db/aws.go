93cc75bcca8d8df4923bc3c71aab104fe08de68e
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-02-27 01:34:54 +0300

    Added DB and Service

---
+++ customer-service/db/aws.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+)
+
+func GetSecretValue() map[string]string {
+	secretName := "rds!db-ab967ce5-63ef-416c-9ad1-a4f712c4b76b"
+	region := "us-east-1"
+
+	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	svc := secretsmanager.NewFromConfig(config)
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"),
+	}
+
+	result, err := svc.GetSecretValue(context.TODO(), input)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+
+	var secretMap map[string]string
+	json.Unmarshal([]byte(*result.SecretString), &secretMap)
+
+	return secretMap
+
+}
