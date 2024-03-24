7b7ba3ffafeed3f2d925ad203727deaa935bd4d6
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-03-24 23:35:27 +0300

    Added UserRepository

---
+++ auth-service/src/main/java/com/customer/auth/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.customer.auth.repository;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.customer.auth.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    private DynamoDBMapper dynamoDBMapper;
+
+    public String createUser(User user) {
+        dynamoDBMapper.save(user);
+        return user.getUsername();
+    }
+
+    public User getUserById(String id) {
+        return dynamoDBMapper.load(User.class, id);
+    }
+}
